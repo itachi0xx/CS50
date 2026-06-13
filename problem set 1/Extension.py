733afd0ab33f010bd2e502029115ee190238ea32
itachi0xx <atiyahnasr@gmail.com>
2026-06-13 18:01:26 +0300

    problem set 1

---
+++ problem set 1/Extension.py
@@ -0,0 +1,16 @@
+def main():
+
+    file = input("File name: ")
+    print(f"Image/{fileEtenssion(file)}")
+    
+
+def fileEtenssion(filename):
+    filename = filename.lower().strip()
+
+    if "." in filename:
+        return filename.split(".")[-1]
+    else:
+        return "None"
+
+    
+main()
