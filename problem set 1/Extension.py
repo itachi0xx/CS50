549222ce2b8c54690a1331a07edf4569d40b0353
itachi0xx <atiyahnasr@gmail.com>
2026-06-16 08:26:18 +0300

    Refactor file extension function and clean up code

--- problem set 1/Extension.py
+++ problem set 1/Extension.py
@@ -1,41 +1,13 @@
 def main():
-    '''
     file = input("File name: ")
-        print(f"Image/{fileEtenssion(file)}")
+    print(f"Image/{fileEtenssion(file)}")
         
 
-    def fileEtenssion(filename):
-        filename = filename.lower().strip()
+def fileEtenssion(filename):
+    filename = filename.lower().strip()
 
-        if "." in filename:
-            return filename.split(".")[-1]
-        else:
-            return "None"
-    '''
-    math = input("Expression: ")
-    math = math.split()
-    lst = ["+", "-", "*", "/"]
-
-    if len(math) != 3:
-        print("Invalid input")
-    elif math[1] not in lst:
-        print("Invalid input")
+    if "." in filename:
+        return filename.split(".")[-1]
     else:
-        x = float(math[0])
-        y = float(math[-1])
-        if math[1] == "+":
-            print(x + y)
-        elif math[1] == "-":
-            print(x - y)
-        elif math[1] == "*":
-            print(x * y)
-        elif math[1] == "/":
-            if y == 0:
-                print("Cannot divide by zero")
-            else:
-                print(x / y)
-
-
-
-    
-main()
+        return "None"
+main()  
