659b5fe59aa620392f523cbd6e8b53fad497dddd
itachi0xx <atiyahnasr@gmail.com>
2026-07-07 23:40:11 +0300

    Grocery List problem

---
+++ problem set 3/grocery.py
@@ -0,0 +1,20 @@
+def main():
+    grocery = {}
+
+    while True:
+        try:
+            item = input().upper()
+
+            if item in grocery:
+                grocery[item] += 1
+            else:
+                grocery[item] = 1
+
+        except EOFError:
+            break
+
+    for item in sorted(grocery):
+        print(grocery[item], item)
+
+
+main()
